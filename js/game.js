c2aa8527d0680f551c5f7262319e85c5e44f86a7
Vinicius Costa Ferreira <133914186+Vinkin-git@users.noreply.github.com>
2023-11-19 12:02:18 -0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -88,7 +88,7 @@ const createCard = (character) => {
   const front = createElement('div', 'face front');
   const back = createElement('div', 'face back');
 
-  front.style.backgroundImage = `url('../images/${character}.png')`;
+  front.style.backgroundImage = `url('img/${character}.jpg')`;
 
   card.appendChild(front);
   card.appendChild(back);
